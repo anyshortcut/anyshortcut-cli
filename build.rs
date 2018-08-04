f0589364bba09a806045e7ce613b6bff7ab548eb
Folyd <lyshuhow@gmail.com>
2018-08-05 00:54:16 +0800

    Custom cargo build to generate shell completion files

---
+++ build.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate clap;
+
+use clap::Shell;
+use std::env;
+
+include!("src/cli.rs");
+
+pub fn main() {
+    let mut app = build_cli();
+    // Generate shell completion files.
+    for shell_name in vec![Shell::Bash, Shell::Zsh, Shell::PowerShell] {
+        app.gen_completions("as", shell_name, env::var_os("OUT_DIR").unwrap());
+    }
+}
