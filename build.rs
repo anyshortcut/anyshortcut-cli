d7dc189172b2f5d70f201ce814afdb920a40e076
Folyd <lyshuhow@gmail.com>
2018-10-14 11:21:04 +0800

    Rename crate name to anyshortcut

--- build.rs
+++ build.rs
@@ -10,6 +10,6 @@ pub fn main() {
     let mut app = build_cli();
     // Generate shell completion files.
     for shell_name in vec![Shell::Bash, Shell::Zsh, Shell::PowerShell] {
-        app.gen_completions("as", shell_name, env::var_os("OUT_DIR").unwrap());
+        app.gen_completions(crate_name!(), shell_name, env::var_os("OUT_DIR").unwrap());
     }
 }
